e19c6c71a26b73dcfededcaa5cb7fd3592543ffe
liquid8796 <hanam.tranle.5@gmail.com>
2026-06-29 02:34:45 +0700

    upd

--- Scripts/Kanto/Leveling/listPokemon.lua
+++ Scripts/Kanto/Leveling/listPokemon.lua
@@ -2,17 +2,17 @@ local listPokemon =
 {
 ['Caterpie']=0,
 ['Ekans']=0,
-['Kakuna']=0,
+['Kakuna']=2,
 ['Ledyba']=0,
 ['Metapod']=0,
-['Noctowl']=1,
+['Noctowl']=2,
 ['Pidgey']=2,
 ['Sentret']=2,
 ['Silcoon']=0,
 ['Snubbull']=0,
 ['Spinarak']=2,
 ['Venonat']=0,
-['Weedle']=1,
+['Weedle']=2,
 ['Yanma']=0,
 }
 return listPokemon
